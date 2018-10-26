b4cf24ea1bc1d539e230b6431bc73a094fe79c71
brettstl <bwlohmeyer@gmail.com>
2018-10-26 08:14:51 -0500

    added files

---
+++ helloworld.py
@@ -0,0 +1,27 @@
+print("hello, world")
+textInput = input("What is your name?:")
+print("Hi", textInput, "!")
+
+num_1 = input("Enter the first number: ")
+num_2 = input("Enter the second number: ")
+result = int(num_1) % int(num_2)
+print("The result is", result)
+
+
+
+
+#print(message)
+# prints a message to the console. 
+# strings need to be within quotes “
+
+#examples
+print(len("Hello")
+print(5)
+
+# Loop through range
+
+#example 
+# i is the counter
+# the last value I think uses a 0 index, so if it is 9, the last number would be 8
+for i in range(1,9):
+  print(i)
